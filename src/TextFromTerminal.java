a990bca46db2b4d66d8ade812ce3e29ea9d29bbc
tzjy <tzjy.gft.com>
2022-11-28 15:59:36 +0100

    Add description to TextFromTerminal.java.

--- src/TextFromTerminal.java
+++ src/TextFromTerminal.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class TextFromTerminal {
-
     public static void main(String[] args) {
+
+         /*
+            Dzięki obiektowi Scannera możemy szczytywać to co użytkownik wpisuje w terminalu.
+         */
+
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         System.out.println("Witaj " + text);
