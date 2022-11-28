998a99e143fe6bc16e75f8b02195c2cce579a3a5
tzjy <tzjy.gft.com>
2022-11-28 12:53:36 +0100

    Add description to ForeachLoop.java.

--- src/ForeachLoop.java
+++ src/ForeachLoop.java
@@ -1,5 +1,13 @@
 public class ForeachLoop {
     public static void main(String[] args) {
+
+        /*
+           Pętla for each to konstrukcja, która pozwala na sekwencyjne przeglądanie różnych zbiorów danych.
+           Mogą nimi być tablice, a także dynamiczne struktury jak na przykład listy.
+
+           https://javastart.pl/baza-wiedzy/java-podstawy-jezyka/petla-for-each
+         */
+
         String[] dogs = new String[]{"Rex", "Scooby", "Szarik", "Sara"};
 
         for (String dog : dogs) {
