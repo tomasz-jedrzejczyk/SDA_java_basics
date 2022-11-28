8eb4ebfdc9cc53afabd98ee9627306f757201717
tzjy <tzjy.gft.com>
2022-11-28 15:52:40 +0100

    Add description to Main.java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+         /*
+            Pierwszy podstawowy program
+         */
+
         System.out.println("Hello world");
     }
 }
