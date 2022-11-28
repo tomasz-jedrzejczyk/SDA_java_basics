7be33bfbb7ba153eab3cdf5767b80ee1be421fb0
tzjy <tzjy.gft.com>
2022-11-28 12:48:37 +0100

    Change Javadoc to block comment in DoWhileLoop.java.

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -1,11 +1,20 @@
 public class DoWhileLoop {
-
     public static void main(String[] args) {
+
+        /*
+           To co znajduje się w jej wnętrzu wykona się przynajmniej raz,
+           ponieważ warunek jest sprawdzany dopiero w drugiej kolejności.
+
+           Tą pętlę można zapamiętać następująco "wykonuj instrukcje dopóki warunek jest prawdziwy"
+
+           https://javastart.pl/baza-wiedzy/java-podstawy-jezyka/petle
+         */
+
         int count = 0;
 
         do{
-            count++;
             System.out.println(count);
+            count++;
         }while (count < 10);
     }
 }
