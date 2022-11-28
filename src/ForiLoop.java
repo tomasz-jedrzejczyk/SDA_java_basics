7de9527a8ef13293aa9356344c055f4595e04015
tzjy <tzjy.gft.com>
2022-11-28 13:00:35 +0100

    Add description to ForiLoop.java.

--- src/ForiLoop.java
+++ src/ForiLoop.java
@@ -1,5 +1,16 @@
 public class ForiLoop {
     public static void main(String[] args) {
+
+         /*
+           Wyrażenie początkowe służy do zainicjowania licznika,
+           zazwyczaj jest w tym samym miejscu zadeklarowana zmienna typu całkowitego -
+           najczęściej oznacza się je przy pomocy liter od "i" wzwyż, jest to przydatne przy zagnieżdżonych pętlach,
+           gdzie przy długich nazwach ciężko by się było połapać. Warunek sprawdza, czy ma się wykonać instrukcja z wnętrza pętli,
+           natomiast modyfikator zmienia licznik - zazwyczaj jest to instrukcja inkrementacji.
+
+           https://javastart.pl/baza-wiedzy/java-podstawy-jezyka/petle#for
+         */
+
         String[] dogs = new String[]{"Rex", "Scooby", "Szarik", "Sara"};
 
         for (int i = 0; i < dogs.length; i++) {
