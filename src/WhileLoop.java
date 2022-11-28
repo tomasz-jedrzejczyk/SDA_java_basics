7348df44ee424c84596b5e48d55a760a76595dd3
tzjy <tzjy.gft.com>
2022-11-28 16:17:14 +0100

    Add description to WhileLoop.java.

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,10 +1,21 @@
 public class WhileLoop {
     public static void main(String[] args) {
+
+        /*
+           Pętlę while najczęściej wykorzystuje się w miejscach,
+           gdzie zakładana ilość powtórzeń jest bliżej nieokreślona, ale znamy warunek jaki musi być spełniony
+
+           Gdy warunek jest spełniony instrukcja we wnętrzu pętli jest wykonywana.
+           Jeżeli natomiast warunek jest fałszywy może się ona nie wykonać, ani razu.
+
+           https://javastart.pl/baza-wiedzy/java-podstawy-jezyka/petle#p%C4%99tla_while
+         */
+
         int count = 0;
 
         while (count < 10){
-            count++;
             System.out.println(count);
+            count++;
         }
     }
 }
